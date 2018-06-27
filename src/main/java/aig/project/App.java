b660bdd4b7c8bdeae1fe06f7c3abb22b99b96250
Subodh <abcd@yahoo.com>
2018-06-27 14:42:51 -0400

    First

---
+++ src/main/java/aig/project/App.java
@@ -0,0 +1,13 @@
+package aig.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/aig/project/AppTest.java
@@ -0,0 +1,30 @@
+package aig.project;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+   public void testApp()
+    {
+    	int a = 4;
+    	
+    	int b= 5;
+    	
+        assertTrue( a+b==9);
+    }
+   
+   public void testApp1()
+   {
+   	int x = 2;
+   	
+   	int y= 2;
+   	
+       assertTrue( x+y==4);
+   }
+}
